5d293c02cdf70d7b7f304e502df3fe21c01b7a7e
Aleksandrina Anna <kurai@vk.com>
2025-09-04 02:28:44 +0300

    Добавила функцию 'get_date' в 'main.py' и обновила все для тестирования

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from src.masks import get_mask_account, get_mask_card_number  # тест
+from src.widget import get_date, mask_account_card
 
-print(get_mask_card_number(7000792289606361))
-print(get_mask_account(73654108430135874305))
+# Тестирование mask_account_card
+print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Счет 73654108430135874305"))
+print(mask_account_card("Maestro 1596837868705199"))
+
+# Тестирование get_date
+print(get_date("2024-03-11T02:26:18.671407"))
