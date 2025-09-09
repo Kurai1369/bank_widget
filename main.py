4663214c7434159fb65478df408d9d613ff39a90
Aleksandrina Anna <kurai@vk.com>
2025-09-09 04:28:30 +0300

    Сортировка по убыванию (по умолчанию)/возрастанию даты, а также сортировка по статусу в main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from src.processing import filter_by_state, sort_by_date
 from src.widget import get_date, mask_account_card
 
 # Тестирование mask_account_card
@@ -7,3 +8,25 @@ print(mask_account_card("Maestro 1596837868705199"))
 
 # Тестирование get_date
 print(get_date("2024-03-11T02:26:18.671407"))
+
+# Тестирование sort_by_date
+operations_data = [
+    {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+    {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+]
+
+# Сортировка по убыванию (по умолчанию)/возрастанию
+sorted_descending = sort_by_date(operations_data)
+print(sorted_descending)
+
+sorted_ascending = sort_by_date(operations_data, descending=False)
+print(sorted_ascending)
+
+# Сортировка по статусу
+sorted_executed = filter_by_state(operations_data)  # EXECUTED
+print(sorted_executed)
+
+sorted_canceled = filter_by_state(operations_data, "CANCELED")  # CANCELED
+print(sorted_executed)
